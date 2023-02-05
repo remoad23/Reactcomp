45a06e826bedff1da2a96cf7038ed69fcbecbe3e
remoad23 <patrick24p@gmail.com>
2023-02-05 17:45:40 +0100

    optimization onclick when trying to redirect to a components page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,32 +4,37 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 function Home() {
     return (
         <div className="mainPageWrapper">
-            <div className="mainPageContent">
+            <div onClick={(e) => Redirect(e.target)}  className="mainPageContent" on>
                 <Link to="pager">Pager</Link>
             </div>
-            <div className="mainPageContent">
+            <div onClick={(e) => Redirect(e.target)}  className="mainPageContent">
                 <Link to="calendar">Calendar</Link>
             </div>
-            <div className="mainPageContent">
+            <div onClick={(e) => Redirect(e.target)}  className="mainPageContent">
                 <Link to="formMaker">FormMaker</Link>
             </div>
-            <div className="mainPageContent">
+            <div onClick={(e) => Redirect(e.target)}  className="mainPageContent">
                 <Link to="chart">Chart</Link>
             </div>
-            <div className="mainPageContent">
+            <div onClick={(e) => Redirect(e.target)}  className="mainPageContent">
                 <Link to="table">Table</Link>
             </div>
-            <div className="mainPageContent">
+            <div onClick={(e) => Redirect(e.target)}  className="mainPageContent">
                 <Link to="toolTip">ToolTip</Link>
             </div>
-            <div className="mainPageContent">
+            <div onClick={(e) => Redirect(e.target)}  className="mainPageContent">
                 <Link to="modalWindow">ModalWindow</Link>
             </div>
-            <div className="mainPageContent">
+            <div onClick={(e) => Redirect(e.target)}  className="mainPageContent">
                 <Link to="fileUploader">FileUploader</Link>
             </div>
         </div>
     );
+}
+
+function Redirect(element) {
+    if (element.firstElementChild)
+        element.firstElementChild.click();
 }
 
 export default Home;
